9520847cc9bb0ad50383942357ecebe51cde983b
ZomoXYZ <ashleyzomo@gmail.com>
2022-03-29 22:31:38 -0500

    init

---
+++ src/discord/types.ts
@@ -0,0 +1,9 @@
+import { MessageEmbed } from "discord.js";
+
+/**
+ * an object that contains embeds and can be passed directly to methods like `Discord.TextChannel.send()`
+ */
+export interface embedDataType {
+    content?: string,
+    embeds: MessageEmbed[]
+}
---
+++ src/types.ts
@@ -0,0 +1,99 @@
+
+
+/**
+ * any indexable object
+ */
+//this is a generic type, and needs 'any'
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type basicObject = {[keys: string]: any};
+
+/**
+ * any indexable object with string values
+ */
+export type basicObjectStringable = {[keys: string]: string|number|boolean|null};
+export type basicObjectString = {[keys: string]: string};
+
+/**
+ * an abstract version of strings
+ */
+export type bigStringType = string | string[];
+
+
+/**
+ * a representation of an author in the LANG object
+ * 
+ * `LANG > Language > Embed > Field`
+ */
+export interface embedField {
+    name: string,
+    value: bigStringType,
+    inline?: boolean
+}
+
+/**
+ * a representation of an author in the LANG object
+ * 
+ * `LANG > Language > Embed > Author`
+ */
+export interface authorData {
+    name: string,
+    url?: string,
+    iconURL?: string
+}
+
+/**
+ * a representation of a footer in the LANG object
+ * 
+ * `LANG > Language > Embed > Footer`
+ */
+export interface footerData {
+    text: string,
+    iconURL?: string
+}
+
+/**
+ * a representation of an embed in the LANG object
+ * 
+ * `LANG > Language > Embed`
+ */
+export interface embedObject {
+    embed: true,
+
+    content?: string,
+    title?: string,
+    description?: bigStringType,
+    /**
+     * URL
+     */
+    url?: string,
+    /**
+     * #FFFFFF
+     */
+    color?: string,
+    footer?: string | footerData,
+    thumbnail?: string,
+    /**
+     * URL
+     */
+    image?: string,
+    /**
+     * URL
+     */
+    author?: string | authorData,
+    fields?: embedField[],
+    timestamp?: boolean | string | number
+}
+
+/**
+ * a specific language in the LANG object
+ * 
+ * `LANG > Language`
+ */
+export type LangObj = { [keys:string]: LangObj | embedObject | string }
+
+/**
+ * the entire LANG object
+ * 
+ * `LANG`
+ */
+export type LangObjWhole = { [langid:string]: LangObj }
